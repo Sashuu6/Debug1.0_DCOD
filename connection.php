80237e73f16581f4936e1b20abee772f4e41b5c7
Sashwat K <sashwat0001@gmail.com>
2018-11-17 11:14:20 +0530

    added code

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-	    $con1 = mysqli_connect("localhost","root","11swatz10");
+	    $con1 = mysqli_connect("localhost","root","password");
 	  //  mysql_select_db("sms",$con);
 	    mysqli_select_db($con1,"debug");
    
